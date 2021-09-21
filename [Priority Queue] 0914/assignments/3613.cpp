b571d6f5f24ca8813ffe844e73e2f8e216ffe5fd
Minji Kang <nitronium@ewhain.net>
2021-09-21 22:25:44 +0900

    [Priority Queue] 9월 21일 - Update

--- [Priority Queue] 0914/assignments/3613.cpp
+++ [Priority Queue] 0914/assignments/3613.cpp
@@ -2,64 +2,126 @@
 // Created by Kang Minji on 2021-09-15.
 //
 
-// 실패
 /*
  * 예외 사항
- * 01. 시작이 _거나 대문자
- * 02. _가 연속으로 나오는 경우
- * 03. _와 대문자가 같이 나오는 경우
- * 04. 마지막이 _인 경우
- *
- * 05. c++인데 대문자 / java인데 _가 나오는 경우
+ * 01. C++
+ *  1) '_'로 시작하거나 끝나는 경우
+ *  2) '__'가 연속해서 등장하는 경우
+ *  3) 대문자가 나오는 경우
+ * 02. Java
+ *  1) 대문자로 시작하는 경우
+ *  2) '_'가 등장하는 경우
  * */
+
 #include <iostream>
 using namespace std;
 
-int main(){
-    string str, ans="";
-    bool isJava, isC, isError = false;
+bool isCpp(string str){ // c++ 형식인가?
+    for (int i=0; i<str.size(); i++){
+        // 대문자가 나오면 안 됨
+        if (isupper(str[i]))
+            return false;
+        // 첫글자나 마지막 글자가 '_' 이거나 '_' 이 연속으로 나오는 경우
+        if (str[i] == '_' && (i==0 || i==str.size() - 1 || str[i-1] == '_'))
+            return false;
+    }
+    return true;
+}
+
+bool isJava(string str){ // java 형식인가?
+    // 첫 글자가 소문자이고 '_'가 없으면 true 반환
+    return !isupper(str[0]) && str.find('_')==string::npos;
+}
+
+string toCpp(string str){ // java -> c++
+    string result;
+    for (int i=0; i<str.size(); i++){
+        if (isupper(str[i])) // 만약 대문자라면 앞에 '_' 넣어주기
+            result += '_';
+        result += tolower(str[i]); // 소문자로 바꿔서 넣기
+    }
+    return result;
+}
+
+string toJava(string str){ // c++ -> java
+    string result;
+    for (int i=0; i<str.size(); i++){
+        if (str[i] == '_'){ // '_' 다음 문자를 대문자로 바꿔서 넣어주기
+            result += toupper(str[i+1]);
+            i++;
+            continue;
+        }
+        result += str[i]; // 다른 문자는 그냥 삽입
+    }
+    return result;
+}
+
+int main() {
+    string str;
     cin >> str;
 
-    // 예외 01. 시작이 '_'이거나 대문자인 경우 + 마지막 문자가 '_'인 경우
-    if (isupper(str[0]) || str[0] == '_' || str[str.length()-1] == '_') {
-        cout << "Error\n";
-        return 0;
-    }
-    ans += str[0];
+    bool is_cpp = isCpp(str);
+    bool is_java = isJava(str);
 
-    for (int i=1; i<str.length(); i++){
-        // 01. c++인 경우('_' 포함)
-        if (str[i] == '_') {
-            // 예외 02. java이거나 '_'이 연속으로 있는 경우
-            if (isJava || str[i-1] == '_'){
-                isError = true;
-                break;
-            }
-            isC = true;
-        }
-            // 02. java인 경우(대문자 포함)
-        else if (isupper(str[i])){
-            // 예외 03. c++인 경우
-            if (isC) {
-                isError = true;
-                break;
-            }
-            isJava = true;
-            // 왜인지 모르겠으나 _랑 tolower이랑 같이 쓰면 같이 이상하게 나옴
-            ans += '_';
-            ans += tolower(str[i]);
-        }
-            // 03. 소문자인 경우
-        else {
-            if (str[i-1] == '_')
-                ans += toupper(str[i]);
-            else
-                ans += str[i];
-        }
-    }
-
-    if (isError)
-        cout << "Error\n";
+    // 01. 두 형식에 부합하는 경우(ex. name)
+    if (is_cpp && is_java)
+        cout << str;
+    // 02. c++이라면 java로 바꿔서 출력
+    else if (is_cpp)
+        cout << toJava(str);
+    // 03. java이면 c++로 바꿔서 출력
+    else if (is_java)
+        cout << toCpp(str);
+    // 04. 둘 다 아니라면 에러 발생
     else
-        cout << ans;
+        cout << "Error!";
 }
+
+//int main(){
+//    string str, ans="";
+//    bool isJava, isC, isError = false;
+//    cin >> str;
+//
+//    // 예외 01. 시작이 '_'이거나 대문자인 경우 + 마지막 문자가 '_'인 경우
+//    if (isupper(str[0]) || str[0] == '_' || str[str.length()-1] == '_') {
+//        cout << "Error\n";
+//        return 0;
+//    }
+//    ans += str[0];
+//
+//    for (int i=1; i<str.length(); i++){
+//        // 01. c++인 경우('_' 포함)
+//        if (str[i] == '_') {
+//            // 예외 02. java이거나 '_'이 연속으로 있는 경우
+//            if (isJava || str[i-1] == '_'){
+//                isError = true;
+//                break;
+//            }
+//            isC = true;
+//        }
+//            // 02. java인 경우(대문자 포함)
+//        else if (isupper(str[i])){
+//            // 예외 03. c++인 경우
+//            if (isC) {
+//                isError = true;
+//                break;
+//            }
+//            isJava = true;
+//            // 왜인지 모르겠으나 _랑 tolower이랑 같이 쓰면 같이 이상하게 나옴
+//            ans += '_';
+//            ans += tolower(str[i]);
+//        }
+//            // 03. 소문자인 경우
+//        else {
+//            if (str[i-1] == '_')
+//                ans += toupper(str[i]);
+//            else
+//                ans += str[i];
+//        }
+//    }
+//
+//    if (isError)
+//        cout << "Error\n";
+//    else
+//        cout << ans;
+//}
--- [Priority Queue] 0914/assignments/7662.cpp
+++ [Priority Queue] 0914/assignments/7662.cpp
@@ -2,63 +2,81 @@
 // Created by Kang Minji on 2021-09-20.
 //
 
-// 실패
+// 성공
+// 실패한 이유 : non-const value 불러오는 데 & 쓰지 않음.
 #include <iostream>
 #include <queue>
+#include <vector>
 using namespace std;
 
-void print_heap( priority_queue<pair<int, int>> max_heap, priority_queue<pair<int, int>, vector<pair<int, int>>, greater<>> min_heap){
-    if (max_heap.empty() && min_heap.empty())
-        cout << "EMPTY\n";
-    else
-        cout << max_heap.top().first << " " << min_heap.top().first << "\n";
+vector<bool> is_valid;
+
+// 최대 힙에서 top 데이터가 is_valid할 때까지 pop
+void deleteMax(priority_queue<pair<int, int>> &heap){
+    while(!heap.empty() && !is_valid[heap.top().second]){
+        heap.pop();
+    }
 }
 
-void sync_heap(bool valid[], priority_queue<pair<int, int>> max_heap, priority_queue<pair<int, int>, vector<pair<int, int>>, greater<>> min_heap){
-    while(!max_heap.empty() && !valid[max_heap.top().second]){
-        max_heap.pop();
-    }
-    while(!min_heap.empty() && !valid[min_heap.top().second]){
-        min_heap.pop();
+// 최소 힙에서 top 데이터가 is_valid할 때까지 pop
+void deleteMin(priority_queue<pair<int, int>, vector<pair<int, int>>, greater<>> &heap){
+    while(!heap.empty() && !is_valid[heap.top().second]){
+        heap.pop();
     }
 }
 
 int main(){
-    // 값, valid 여부(0, 1)
-    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<>> min_heap; // 오름차순
-    priority_queue<pair<int, int>> max_heap; // 내림차순
     int t, k, n;
     char op;
-    bool valid[1000001] = {true, };
     cin >> t;
 
     while(t--){
+        // 값, valid 여부(0, 1)
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<>> min_heap; // 최소 힙
+        priority_queue<pair<int, int>> max_heap; // 최대 힙
+
         cin >> k;
-        for (int i=0; i<k; i++){
+        int idx = 0; // 연산으로 입력된 숫자의 인덱스
+        while(k--){
+            // 입력
             cin >> op >> n;
-            sync_heap(valid, max_heap, min_heap);
-            // 01. 삽입 연산
-            if (op == 'I'){
-                min_heap.push({n, i});
-                max_heap.push({n, i});
-            }
+
+            switch (op){
+                // 01. 삽입 연산
+                case 'I': // 두 힙 모두에 값을 넣은 후, valid에도 추가
+                    min_heap.push({n, idx});
+                    max_heap.push({n, idx});
+                    is_valid.push_back(true);
+                    idx++;
+                    break;
+                case 'D':
                 // 02. 삭제 연산
-            else if (op == 'D'){
-                if (n == 1){ // 최댓값 삭제
-                    if (!max_heap.empty()){
-                        valid[max_heap.top().second] = false;
-                        max_heap.pop();
+                    if (n == 1){ // 최댓값 삭제
+                        deleteMax(max_heap); // D 연산 전처리
+                        if (!max_heap.empty()) { //D연산
+                            is_valid[max_heap.top().second] = false;
+                            max_heap.pop();
+                        }
                     }
-                }
-                else if (n == -1) { // 최솟값 삭제
-                    if (!min_heap.empty()){
-                        valid[min_heap.top().second] = false;
-                        min_heap.pop();
+                    if (n == -1) { // 최솟값 삭제
+                        deleteMin(min_heap); // D 연산 전처리
+                        if (!min_heap.empty()) { //D연산
+                            is_valid[min_heap.top().second] = false;
+                            min_heap.pop();
+                        }
                     }
-                }
+                    break;
             }
         }
-        // 03. 최댓값 최솟값 출력
-        print_heap(max_heap, min_heap);
+        // 각 힙의 top이 valid하도록 처리
+        deleteMax(max_heap);
+        deleteMin(min_heap);
+
+        // 출력
+        if (max_heap.empty())
+            cout << "EMPTY\n";
+        else
+            cout << max_heap.top().first << " " << min_heap.top().first << "\n";
+        is_valid.clear(); // 새로운 테스트 케이스 전 벡터 초기화
     }
 }
