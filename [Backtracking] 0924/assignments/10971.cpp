7bdc2324745dbf642f9cd03d1da5b9cbdd999cf2
Minji Kang <nitronium@ewhain.net>
2021-09-30 19:10:56 +0900

    [Backtracking] 9월 30일 -Update

--- [Backtracking] 0924/assignments/10971.cpp
+++ [Backtracking] 0924/assignments/10971.cpp
@@ -14,13 +14,17 @@ int min_cost = 2147483647;
 // start : 시작 도시, end : 도착 도시
 // cnt : 방문한 도시의 수, cost : 누적 비용
 void backtracking(int start, int end, int cnt, int cost){
-    // 모든 도시를 다 방문했는가? + 처음 도시로 돌아갈 수 있는가?
-    if (cnt == n-1 && weight[start][end] != 0){
-        cost += weight[start][end]; // 처음 도시로 돌아가는 비용
-        min_cost = min(min_cost, cost);
+    // 03. 모든 도시를 다 방문한 경우
+    if (cnt == n-1){
+        // 처음 도시로 돌아갈 수 있는 경우
+        if (weight[start][end] != 0) {
+            cost += weight[start][end]; // 처음 도시로 돌아가는 비용
+            min_cost = min(min_cost, cost);
+        }
+        // 처음 도시로 돌아갈 수 없는 경우 -> cost값 update하지 않고 바로 리턴?
         return;
     }
-    // 나머지 도시를 하나씩 방문
+    // 02. 나머지 도시를 하나씩 방문
     for (int i=0; i<n; i++){
         // 아직 방문하지 않은 도시 + 현재 도시에서 갈 수 있는 도시인 경우
         if (!check[i] && weight[start][i] != 0){
@@ -44,7 +48,7 @@ int main(){
             cin >> weight[i][j];
 
     // 연산 + 출력
-    // 시작 도시 별로 탐색하되, 시작과 끝이 모두 하나의 도시로!
+    // 01. 시작 도시 별로 탐색하되, 시작과 끝이 모두 하나의 도시로!
     for (int i=0; i<n; i++){
         check[i] = true;
         backtracking(i, i, 0, 0);
